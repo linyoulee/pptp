76b2818a92ec19aba5c0c78744b4b33aa7e1a144
linyoulee <linyoulee@gmail.com>
2014-03-17 17:29:46 +0800

    Update pptpd6.sh

--- pptpd6.sh
+++ pptpd6.sh
@@ -1,16 +1,3 @@
-yum remove -y pptpd ppp
-iptables --flush POSTROUTING --table nat
-iptables --flush FORWARD
-rm -rf /etc/pptpd.conf
-rm -rf /etc/ppp
-
-arch=`uname -m`
-wget http://poptop.sourceforge.net/yum/stable/packages/pptpd-1.4.0-1.el6.$arch.rpm
-
-yum -y install make libpcap iptables gcc-c++ logrotate tar cpio perl pam tcp_wrappers dkms kernel_ppp_mppe ppp
-rpm -Uvh pptpd-1.4.0-1.el6.$arch.rpm
-
-
 mknod /dev/ppp c 108 0 
 echo 1 > /proc/sys/net/ipv4/ip_forward 
 echo "mknod /dev/ppp c 108 0" >> /etc/rc.local
